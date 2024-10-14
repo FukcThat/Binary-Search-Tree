105c435cae2ea039eb706b68c4c0ca0c5e969954
FukcThat <63652e636f6465@gmail.com>
2024-10-14 16:03:16 +0200

    FEAT: Build Tree fromm Array functionality added

--- Second Tree/Tree.mjs
+++ Second Tree/Tree.mjs
@@ -1,33 +1,72 @@
 import Node from "./Node.mjs";
 
 class Tree {
-  constructor(array) {}
+  constructor(array) {
+    this.root = this.buildTree(array);
+  }
+
+  // Build Tree
+  buildTree = (array) => {
+    // Sort Array
+    array.sort((a, b) => {
+      return a - b;
+    });
+
+    // Remove duplicates from Array
+    let removeDuplicatesSet = new Set(array);
+    array = Array.from(removeDuplicatesSet);
+
+    // Recursively go left and right respectively (helper)
+    this.buildFromArray(array, 0, array.length - 1);
+  };
+
+  // Build Tree Helper
+  buildFromArray = (array, start, end) => {
+    // if the start for this function is greater than the end, stop
+    if (start > end) return null;
+
+    // Find center of array & make a new Node
+    let centerIndex = Math.floor((start - end) / 2);
+    const newNode = new Node(array[centerIndex]);
+
+    // Recursively make new left & right nodes
+    newNode.left = this.buildFromArray(array, start, centerIndex);
+    newNode.left = this.buildFromArray(array, centerIndex.end);
+
+    // return the newNode (which is the root for the build tree function)
+    return newNode;
+  };
+
+  // Insert (value)
+
+  // Find Item(value)
+
+  // Delete Item(value)
+
+  // Find min. Node
+
+  // Level Order
+
+  // In Order
+
+  // Pre Order
+
+  // Post Order
+
+  // Depth
+
+  // Height
+
+  // Is Balances
+
+  // Rebalance
+
+  // Pretty Print
+  prettyPrint = (node = this.root, prefix = "", isLeft = true) => {
+    if (node !== null) {
+      console.log(prefix + (isLeft ? "├── " : "└── ") + node.value);
+      this.prettyPrint(node.left, prefix + (isLeft ? "│   " : "    "), true);
+      this.prettyPrint(node.right, prefix + (isLeft ? "│   " : "    "), false);
+    }
+  };
 }
-
-// Build Tree
-
-// Build Tree Helper
-
-// Insert (value)
-
-// Find Item(value)
-
-// Delete Item(value)
-
-// Find min. Node
-
-// Level Order
-
-// In Order
-
-// Pre Order
-
-// Post Order
-
-// Depth
-
-// Height
-
-// Is Balances
-
-// Rebalance
---
+++ Second Tree/index.mjs
@@ -0,0 +1,5 @@
+import Tree from "../First Tree/Tree.mjs";
+
+const newTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+newTree.prettyPrint();
