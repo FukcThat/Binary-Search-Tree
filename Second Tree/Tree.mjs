13d9ce689bfe22e8d4c6ce484d0c49477c412fda
FukcThat <63652e636f6465@gmail.com>
2024-10-14 15:03:39 +0200

    Repeat: Git init

---
+++ Second Tree/Tree.mjs
@@ -0,0 +1,33 @@
+import Node from "./Node.mjs";
+
+class Tree {
+  constructor(array) {}
+}
+
+// Build Tree
+
+// Build Tree Helper
+
+// Insert (value)
+
+// Find Item(value)
+
+// Delete Item(value)
+
+// Find min. Node
+
+// Level Order
+
+// In Order
+
+// Pre Order
+
+// Post Order
+
+// Depth
+
+// Height
+
+// Is Balances
+
+// Rebalance
